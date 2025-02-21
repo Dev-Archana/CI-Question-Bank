a0f1abe8e9c2a42bc486068c3366cbc6d9c405a7
Archana <167700685+Dev-Archana@users.noreply.github.com>
2025-02-21 16:37:28 +0530

    Create squirrelPlay.java

---
+++ squirrelPlay.java
@@ -0,0 +1,12 @@
+public boolean squirrelPlay(int temp, boolean isSummer) {
+  if(isSummer)
+  return 60<=temp&&temp<=100;
+  else
+  return 60<=temp&&temp<=90;
+}
+
+/*
+    platform : CodingBat -> Logic-1
+    Question Name: cigarParty
+    Author : Archana
+*/
