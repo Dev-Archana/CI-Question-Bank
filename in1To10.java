5d964b06a358d9c4b534eb7d7e73a55ca71e3b58
Archana <167700685+Dev-Archana@users.noreply.github.com>
2025-02-21 20:24:21 +0530

    Create in1To10.java

---
+++ in1To10.java
@@ -0,0 +1,16 @@
+public boolean in1To10(int n, boolean outsideMode) {
+  if(outsideMode)
+  {
+    return n<=1||n>=10;
+  }
+  else 
+  {
+  return n>=1&&n<=10;
+  }
+}
+
+/*
+    platform : CodingBat -> Logic-1
+    Question Name: in1To10
+    Author : Archana
+*/
