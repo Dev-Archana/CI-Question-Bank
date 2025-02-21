e963fccd17965abd2541496818ddd79e951b3d3a
Archana <167700685+Dev-Archana@users.noreply.github.com>
2025-02-21 20:27:38 +0530

    Create specialEleven.java

---
+++ specialEleven.java
@@ -0,0 +1,14 @@
+public boolean specialEleven(int n) {
+  if(n%11==0||n%11==1)
+  {
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+/*
+    platform : CodingBat -> Logic-1
+    Question Name: specialEleven
+    Author : Archana
+*/
